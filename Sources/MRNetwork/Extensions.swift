8d49752074b853e9ef9dbc81396f68572a8f62d6
Miguel <ridruejomiguel@gmail.com>
2023-12-03 20:50:26 +0100

    Network Library Creation

---
+++ Sources/MRNetwork/Extensions.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by Miguel Ridruejo on 3/12/23.
+//
+
+import Foundation
+
+public extension URLSession {
+    func dataRequest(from url:URL) async throws -> (Data, URLResponse) {
+        do {
+            return try await data(from: url)
+        } catch {
+            throw NetworkError.general(error)
+        }
+    }
+    
+    func dataRequest(for request:URLRequest) async throws -> (Data, URLResponse) {
+        do {
+            return try await data(for: request)
+        } catch {
+            throw NetworkError.general(error)
+        }
+    }
+}
---
+++ Sources/MRNetwork/URLRequest.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Miguel Ridruejo on 3/12/23.
+//
+
+import Foundation
